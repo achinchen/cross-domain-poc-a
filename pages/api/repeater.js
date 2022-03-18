1b7cb1600c61470b1096d1a43a61a2dd9743c230
Chin <greeting.to.chin@gmail.com>
2022-03-18 13:51:07 +0800

    feat(api/repeater): add CORS repeater endpoint

---
+++ pages/api/repeater.js
@@ -0,0 +1,42 @@
+const DEFAULT_ALLOW_METHODS = [
+  'POST',
+  'GET',
+  'PUT',
+  'PATCH',
+  'DELETE',
+  'OPTIONS'
+]
+
+const DEFAULT_ALLOW_HEADERS = [
+  'X-Requested-With',
+  'Access-Control-Allow-Origin',
+  'X-HTTP-Method-Override',
+  'Content-Type',
+  'Authorization',
+  'Accept'
+]
+
+const DEFAULT_MAX_AGE_SECONDS = 60 * 60 * 24 // 24 hours
+const MAX_AGE_FOR_COOKIE = 60 * 30 // 30 mins
+
+
+const setHeaderForPreflight = (request, response) => {
+  const preflight = request.method === 'OPTIONS'
+  if (preflight) {
+    response.setHeader('Access-Control-Allow-Methods', DEFAULT_ALLOW_METHODS.join(','))
+    response.setHeader('Access-Control-Allow-Headers', DEFAULT_ALLOW_HEADERS.join(','))
+    response.setHeader('Access-Control-Max-Age', String(DEFAULT_MAX_AGE_SECONDS))
+  }
+}
+
+const setHeaderForCORS = (response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*')
+}
+
+
+export default function handler(request, response) {
+  setHeaderForPreflight(request, response)
+  setHeaderForCORS(response)
+  response.setHeader('set-cookie', `delicious=from_site_a_endpoint; path=/; secure; maxAge=${MAX_AGE_FOR_COOKIE}`)
+  response.redirect(302, 'https://sitea.achin.dev')
+}
